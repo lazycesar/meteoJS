4e641f9fe0a58f02cfe071715435da60e9108239
lazycesar <cesarespi36@hotmail.com>
2018-11-16 12:13:08 +0100

    styling changes

--- js/style.js
+++ js/style.js
@@ -12,7 +12,7 @@ const meteos = {
   },
 
   Clear: {
-        icone: '<i class="wi wi-day-sunny"></i>',
+    icone: '<i class="wi wi-day-sunny"></i>',
     commentaire: "Dégagé",
     bgstyle: "#2889D7"
   },
@@ -42,8 +42,6 @@ const meteos = {
 };
 
 function miseEnFormeMeteoDuJour(meteoJour) {
-  
-  
   const fleche = tourneFleche(meteoJour.wind.deg);
   const temps = meteoJour.weather[0].main;
   const leveSoleil =
@@ -51,12 +49,21 @@ function miseEnFormeMeteoDuJour(meteoJour) {
   const coucheSoleil =
     "<i class='wi wi-sunset'>" + convertDate(+meteoJour.sys.sunset).heure;
   const releve = convertDate(+meteoJour.dt).heure;
-  const formatVillePourTitre = afficheVilleCountry().slice(0, afficheVilleCountry().indexOf(","))
-  document.title = formatVillePourTitre+" : " + meteos[temps].commentaire+"/"+meteoJour.main.temp + " °C"
- setTimer();
+  const formatVillePourTitre = afficheVilleCountry().slice(
+    0,
+    afficheVilleCountry().indexOf(",")
+  );
+  document.title =
+    formatVillePourTitre +
+    " : " +
+    meteos[temps].commentaire +
+    "/" +
+    meteoJour.main.temp +
+    " °C";
+  setTimer();
 
-  document.getElementById("infos").innerHTML = miseEnParagraphe(
-    "Actuellement à " + afficheVilleCountry()
+  document.getElementById("infoVille").innerHTML = miseEnParagraphe(
+    "Actuellement à " + "<p>" + afficheVilleCountry() + "</p>"
   );
   document.getElementById("commentaire").innerHTML = miseEnParagraphe(
     meteos[temps].commentaire
@@ -71,80 +78,74 @@ function miseEnFormeMeteoDuJour(meteoJour) {
     miseEnParagraphe("Pression : " + meteoJour.main.pressure + " hP") +
     miseEnParagraphe("Taux d'humidité : " + meteoJour.main.humidity + " %") +
     miseEnParagraphe(leveSoleil + " " + coucheSoleil);
-  document.getElementById("vent").innerHTML =
-    miseEnParagraphe(
-      "Vent : " + meteoJour.wind.speed + " km/h - direction " + fleche
-    ) + miseEnParagraphe("mise à jour : " + releve);
-
-
+  document.getElementById("vent").innerHTML = miseEnParagraphe(
+    "Vent : " + meteoJour.wind.speed + " km/h - direction " + fleche
+  );
+  document.getElementById("jsDate").innerHTML = miseEnParagraphe(
+    "mise à jour : " + releve
+  );
 }
 
 function miseEnFormeForcast(forecast) {
-  
   /////// section tendance du jour
 
- document.getElementById("nexthours").innerHTML = createForcast(forecast.meteoJour);
+  document.getElementById("nexthours").innerHTML = createForcast(
+    forecast.meteoJour
+  );
 
-//   /////// section jours suivant
-let listeArticle =""
+  //   /////// section jours suivant
+  let listeArticle = "";
 
-for (i = 0; i < forecast.forecast.length; i += 8) {
-  
-  
-  listeArticle +="<section>"+miseEnParagraphe(convertDate(forecast.forecast[i].dt).date)+createForcast(forecast.forecast.slice(i,i+7))+"</section>";
+  for (i = 0; i < forecast.forecast.length; i += 8) {
+    listeArticle +=
+      "<section>" +
+      miseEnParagraphe(convertDate(forecast.forecast[i].dt).date) +
+      createForcast(forecast.forecast.slice(i, i + 7)) +
+      "</section>";
   }
 
   document.getElementById("nextdays").innerHTML = listeArticle;
+}
 
- }
+function afficheTopVilles(topVilles) {
+  html = "<ol>";
+  let ville = "";
+  let pays = "";
 
-
-function afficheTopVilles(topVilles){
-  html ="<ol>";
-  let ville ="";
-  let pays ="";
-
-  for(i=0;i<topVilles.length;i++){
-    ville = capitalize(topVilles[i].ville)
+  for (i = 0; i < topVilles.length; i++) {
+    ville = capitalize(topVilles[i].ville);
     pays = topVilles[i].country.toUpperCase();
 
-    
-    html += `<li><button data-action="afficher" data-ville="${ville+","+pays}">${ville+","+pays}</button></li>`
-    
+    html += `<li><button data-action="afficher" data-ville="${ville +
+      "," +
+      pays}">${ville + "," + pays}</button></li>`;
   }
 
-  html+="<ol>"
+  html += "<ol>";
   document.getElementById("topvilles").innerHTML = html;
   var matches = document.querySelectorAll('button[data-action="afficher"]');
 
-  matches.forEach(link=>{
-    link.addEventListener('click', chargeMeteoVille)
-  })
+  matches.forEach(link => {
+    link.addEventListener("click", chargeMeteoVille);
+  });
 }
 
 function erreurVille() {
-
- 
   document.getElementById("ville").style.border = "2px solid red";
-
 }
 
-function createForcast(tableau){
-  let html ="";
+function createForcast(tableau) {
+  let html = "";
   // console.log(tableau)
-  for(j=0;j<tableau.length;j++){
-  
-  icone = meteos[tableau[j].weather[0].main].icone
-  html +=
-  "<article>" +
-  miseEnParagraphe(convertDate(tableau[j].dt).heure) +
+  for (j = 0; j < tableau.length; j++) {
+    icone = meteos[tableau[j].weather[0].main].icone;
+    html +=
+      "<article>" +
+      miseEnParagraphe(convertDate(tableau[j].dt).heure) +
+      miseEnParagraphe(icone) +
+      miseEnParagraphe(tableau[j].main.temp + " °C") +
+      "</article>";
+  }
 
-  miseEnParagraphe(icone)+
-
-  miseEnParagraphe(tableau[j].main.temp + " °C")
-
-+"</article>";
-}
-
-return html
+  return html;
 }
